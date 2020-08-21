7a3d512e8e848c6cd26353a4f8374fcdb2936b76
Austin Condict <acondict3@gatech.edu>
2020-08-20 21:13:53 -0400

    Finally looks to be a semi-functional but ugly solution

--- loopback_process.py
+++ loopback_process.py
@@ -257,13 +257,13 @@ class VLCAudioListener(VLCAudioBase):
 		self.clip_format = capture_format.lower()
 		# self.clip_ext = f".{self.clip_format}"
 		self.clip_len = capture_duration
-		self.unprocessed_clips = list()
+		self.unprocessed_clips = Queue()  # list()
 
 	@property
 	def clip_filename(self):
 		## TODO: Perhaps just return f"output.{self.clip_format}" without performing any checks for filename collisions
 		clip_num = 0
-		while f"output{clip_num}.{self.clip_format}" in (os.listdir() + self.unprocessed_clips):
+		while f"output{clip_num}.{self.clip_format}" in (os.listdir()):  # + list(self.unprocessed_clips)):
 			clip_num += 1
 		self.__current_clip_name = f"output{clip_num}.{self.clip_format}"  # os.path.join(os.getcwd(), f"output{clip_num}.{self.clip_format}")
 		# return self.__current_clip_name
@@ -290,7 +290,8 @@ class VLCAudioListener(VLCAudioBase):
 		if not self.is_running:
 			cmd = self.listen_cmd if not use_shlex else shlex.split(self.listen_cmd)
 			print(f"[listen_start]  Listener '{self.name}' recording new clip:  '{self.__current_clip_name}'")
-			self.unprocessed_clips.append(self.__current_clip_name)
+			# self.unprocessed_clips.append(self.__current_clip_name)
+			self.unprocessed_clips.put(self.__current_clip_name)
 			self.process = subprocess.Popen(cmd, shell=use_shell)
 			self.update_state("RECORDING")
 		else:
@@ -317,9 +318,11 @@ class VLCAudioListener(VLCAudioBase):
 
 	### TODO: ^ Abstract the process stop/kill method out into the VLCAudioBase class, as it's really the same for both the Streamer & Listener objects
 
+	"""
 	def register_file_as_processed(self, filename):
 		if filename in self.unprocessed_clips:
 			self.unprocessed_clips.remove(filename)
+	"""
 
 ##=============================================================================
 
@@ -341,13 +344,6 @@ class YetiManager():
 		self.filename = None
 
 		## TODO: Read these configuration values in from a file
-		self.CODEC = "mpga"  #"s16l"
-		self.CHANNELS = 2  #1
-		self.SAMPLERATE = 44100  #48000
-		self.BITRATE = 256  #128
-		self.settings = VLCAudioSettings(self.stream_mrl, self.loop_mrl, self.CODEC, self.CHANNELS, self.SAMPLERATE, self.BITRATE)
-
-		## TODO: Read these configuration values in from a file
 		self.stream_rtp_addr = "239.255.12.42"
 		self.stream_rtp_port = 1234
 		self.loopback_addr = "127.0.0.1" 	## <-- Address to listen on for stream audio processing/saving
@@ -356,6 +352,13 @@ class YetiManager():
 		self.recording_format = "wav"
 		self.verbose_level = 0  	## 0 = -q, 1 = -v, 2 = -vv, 3 = -vvv
 		self.streaming_protocol = "rtp"
+
+		## TODO: Read these configuration values in from a file
+		self.CODEC = "mpga"  #"s16l"
+		self.CHANNELS = 2  #1
+		self.SAMPLERATE = 44100  #48000
+		self.BITRATE = 256  #128
+		self.settings = VLCAudioSettings(self.stream_mrl, self.loop_mrl, self.CODEC, self.CHANNELS, self.SAMPLERATE, self.BITRATE)
 
 
 		self.streamer = VLCAudioStreamer(self.stream_name, self.settings, self.stream_rtp_addr, dest_port=self.stream_rtp_port, 
@@ -375,22 +378,23 @@ class YetiManager():
 		self.post_queue = Queue()
 		self.hash_queue = Queue()
 		# self.frame_q = Queue()
-        # self.kafka_hash_q = Queue()  # Needed since producers cannot be shared across processes
+		# self.kafka_hash_q = Queue()  # Needed since producers cannot be shared across processes
 
-        ## Processes
-        print(f'[{self.__class__}]  Initializing Audio Process')
-        self.audio_process = Process(target=self.audio_processing_loop, args=(self.hash_queue))   # args=(self.frame_q, self.p, self.duration_lock, self.do_calibration_flag, self.calibration_lock))
-        print(f'[{self.__class__}]  Initializing Hash Process')
-        self.hash_process = Process(target=self.frames_to_hash, args=(self.frame_q, self.post_queue))
+		## Processes
+		print(f'[{self.__class__}]  Initializing Audio Process')
+		self.audio_process = Process(target=self.audio_processing_loop, args=(self.hash_queue,))   # args=(self.frame_q, self.p, self.duration_lock, self.do_calibration_flag, self.calibration_lock))
+	   
+		print(f'[{self.__class__}]  Initializing Hash Process')
+		self.hash_process = Process(target=self.hash_rename, args=(self.hash_queue, self.post_queue))
 
-        print(f'[{self.__class__}]  Initializing Posting Process')
-        self.posting_process = Process(target=self.post_cdn, args=(self.post_queue, self.kafka_hash_q))
+		print(f'[{self.__class__}]  Initializing Posting Process')
+		self.posting_process = Process(target=self.post_cdn, args=(self.post_queue,))  # , self.kafka_hash_q))
 
-        ## Set all process daemons
-        print(f'[{self.__class__}]  Setting all processes to daemon=True')
-        self.audio_process.daemon = True
-        self.hash_process.daemon = True
-        self.posting_process.daemon = True
+		## Set all process daemons
+		print(f'[{self.__class__}]  Setting all processes to daemon=True')
+		self.audio_process.daemon = True
+		self.hash_process.daemon = True
+		self.posting_process.daemon = True
 
 
 	@property
@@ -412,130 +416,158 @@ class YetiManager():
 		return f"rtp://@{self.loopback_addr}:{self.loopback_port}"
 	
 	def audio_processing_loop(self, hash_q):
+		print('Audio Process Successfully Started')
 		self.streamer.stream_start()
 		while True:
 		# try:
-			self.start_time = _get_timestamp()
+			start_time = _get_timestamp()
 			ts = time.time()
 			self.listener.listen_start()
 			while (time.time() - ts) <= int(self.listener.clip_len + 1):
 				time.sleep(0.1)
 			self.listener.listen_stop()
-			self.end_time = _get_timestamp()
+			end_time = _get_timestamp()
 			## TODO: Use queue instead & ensure that all unprocessed_clips are processed & removed
-			audio_clip = self.hash_rename(self.listener.unprocessed_clips[0])
-			self.add_to_post_q(audio_clip)
-			# Clear the start and end timestamps
-		    self.start_time = ''
-		    self.end_time = ''
+			# audio_clip = self.hash_rename(self.listener.unprocessed_clips[0])
+			audio_clip_name = self.listener.unprocessed_clips.get()  #[0]
+			hash_q.put((audio_clip_name, start_time, end_time))
+			# self.add_to_post_q(audio_clip)
+			## Clear the start and end timestamps
+			# self.start_time = ''
+			# self.end_time = ''
 			# except KeyboardInterrupt:
 			# 	break
 		
-
 	def kill_all_vlc(self):
 		print(f"\n[{self.__class__}]  Aborting: Terminating all VLC activities.")
 		self.listener.listen_stop()
 		self.streamer.stream_stop()
 	
-	def hash_rename(self, old_filename):
-		# try:
-		with open(old_filename, 'rb') as f:
-			audio_data = f.read()
-			h = hashlib.new('sha1', audio_data)
-			new_filename = h.hexdigest() + f".{self.recording_format}"  # self.listener.clip_format}"
-		os.rename(old_filename, new_filename)
-		print(f"[hash_rename]  Audio file '{old_filename}' has been renamed to '{new_filename}'")
-		self.listener.register_file_as_processed(old_filename)
-		return new_filename
+	def hash_rename(self, hash_q, post_q):  #old_filename):
+		print('Hash Process Successfully Started')
+		while True:
+			while not hash_q.empty():
+				# try:
+				unprocessed_data = hash_q.get()
+				old_filename = unprocessed_data[0]
+				self.start_time = unprocessed_data[1]
+				self.end_time = unprocessed_data[2]
+				with open(old_filename, 'rb') as f:
+					audio_data = f.read()
+					h = hashlib.new('sha1', audio_data)
+					new_filename = h.hexdigest() + f".{self.recording_format}"  # self.listener.clip_format}"
+				os.rename(old_filename, new_filename)
+				print(f"[hash_rename]  Audio file '{old_filename}' has been renamed to '{new_filename}'")
+				# self.listener.register_file_as_processed(old_filename)
+				# return new_filename
 
+				filesize = os.path.getsize(new_filename)
+				## Put all the data into the posting queue as a dictionary for easy unpacking
+				post_q.put({
+					"filename": new_filename,
+					"file_size": filesize,
+					"sha": new_filename.split('.')[0],
+					"start_t": self.start_time,
+					"end_t": self.end_time,
+					# "calibration": calibration_flag
+					})
+
+				## Clear the start and end timestamps
+				self.start_time = ''
+				self.end_time = ''
+				
+	"""
 	def add_to_post_q(self, filename):
 		filesize = os.path.getsize(filename)
-        # put all the data into the posting queue as a dictionary for easy unpacking
-        self.post_queue.put({
-            "filename": filename,
-            "file_size": filesize,
-            "sha": filename.split('.')[0],
-            "start_t": self.start_time,
-            "end_t": self.end_time,
-            # "calibration": calibration_flag
-            })
+		# put all the data into the posting queue as a dictionary for easy unpacking
+		# self.post_queue.put({
+		post_queue.put({
+			"filename": filename,
+			"file_size": filesize,
+			"sha": filename.split('.')[0],
+			"start_t": self.start_time,
+			"end_t": self.end_time,
+			# "calibration": calibration_flag
+			})
+	"""
 
-    def post_to_cdn(self):
-    	while True:
-    		while not self.post_queue.empty():
-    			message = post_queue.get()
-    			## Get the associated info from the message
-    			# calibration_flag = message["calibration"]
-                filename = message["filename"]
-                filesize = message["file_size"]
-                sha = message["sha"]
-                start_time = message["start_t"]
-                end_time = message["end_t"]
+	def post_cdn(self, post_q):
+		print('Posting Process Successfully Started')
+		while True:
+			while not post_q.empty():
+				message = post_q.get()
+				## Get the associated info from the message
+				# calibration_flag = message["calibration"]
+				filename = message["filename"]
+				filesize = message["file_size"]
+				sha = message["sha"]
+				start_time = message["start_t"]
+				end_time = message["end_t"]
 
-                print(f"[MOCK::post_to_cdn]  Posting data to CDN: {message}")
-                time.sleep(1)
-                print(f"[MOCK::post_to_cdn]  Post to CDN was successful --> removing file '{filename}'")
-                os.remove(filename)
-                """
-                try:
-                    files = {'files': open(filename, 'rb')}
-                    response = requests.post(f'http://{cdn_url}:{cdn_port}/upload', files=files)
-                    fileid = response.text.split()[-1]
+				print(f"[MOCK::post_to_cdn]  Posting data to CDN: {message}")
+				time.sleep(1)
+				print(f"[MOCK::post_to_cdn]  Post to CDN was successful --> removing file '{filename}'")
+				os.remove(filename)
+				"""
+				try:
+					files = {'files': open(filename, 'rb')}
+					response = requests.post(f'http://{cdn_url}:{cdn_port}/upload', files=files)
+					fileid = response.text.split()[-1]
 
-                    # Ensure SHA posted matches the current file's SHA
-                    if fileid != sha:
-                        self.my_logger.error(f'SHA mismatch error!: file:{sha}, POST:{fileid}')
-                    confirmation = requests.get(f'http://{cdn_url}:{cdn_port}/{fileid}')
-                    if str(confirmation.status_code) != '200':
-                        self.my_logger.error(f'Upload error: HTTP {confirmation.status_code}')
-                        '''
-                        If POST failed, need to alert if the recorder starts filling up with .wavs
-                        The process will exit if the device runs out of space.
-                        '''
-                        usage = shutil.disk_usage('/')
-                        percent_used = usage[1] / usage[0] * 100
-                        self.my_logger.info('Recording not deleted. System storage used: %.2f%%' % percent_used)
-                        if percent_used > 95:
-                            self.my_logger.critical('Microphone crash imminent: Storage used: %.2f%%' % percent_used)
-                        elif percent_used > 90:
-                            self.my_logger.warning('System nearly full. Storage used: %.2f%%' % percent_used)
+					# Ensure SHA posted matches the current file's SHA
+					if fileid != sha:
+						self.my_logger.error(f'SHA mismatch error!: file:{sha}, POST:{fileid}')
+					confirmation = requests.get(f'http://{cdn_url}:{cdn_port}/{fileid}')
+					if str(confirmation.status_code) != '200':
+						self.my_logger.error(f'Upload error: HTTP {confirmation.status_code}')
+						'''
+						If POST failed, need to alert if the recorder starts filling up with .wavs
+						The process will exit if the device runs out of space.
+						'''
+						usage = shutil.disk_usage('/')
+						percent_used = usage[1] / usage[0] * 100
+						self.my_logger.info('Recording not deleted. System storage used: %.2f%%' % percent_used)
+						if percent_used > 95:
+							self.my_logger.critical('Microphone crash imminent: Storage used: %.2f%%' % percent_used)
+						elif percent_used > 90:
+							self.my_logger.warning('System nearly full. Storage used: %.2f%%' % percent_used)
 
-                    elif fileid == sha:  # and str(confirmation.status_code) == '200' implied
-                        os.remove(filename)
-                        self.my_logger.info("Post to CDN was successful")
-                        self.my_logger.info("Deleted file: {}".format(filename))
+					elif fileid == sha:  # and str(confirmation.status_code) == '200' implied
+						os.remove(filename)
+						self.my_logger.info("Post to CDN was successful")
+						self.my_logger.info("Deleted file: {}".format(filename))
 
-                        kafka_hash_q.put({'text': f'{filename}', 'details': {"startTime": str(start_time),
-                                                                             "endTime": str(end_time),
-                                                                             "SHA1": f'{filename}',
-                                                                             "fileSize": str(filesize),
-                                                                             "Room": self.room,
-                                                                             "microphone": self.microphone_number,
-                                                                             "calibration_flag": calibration_flag}})
-                except NewConnectionError as e:
-                    self.my_logger.error(f'Couldn\'t resolve address: {e}')
-                    tb = traceback.format_exc()
-                    self.my_logger.error(tb)
+						kafka_hash_q.put({'text': f'{filename}', 'details': {"startTime": str(start_time),
+																			 "endTime": str(end_time),
+																			 "SHA1": f'{filename}',
+																			 "fileSize": str(filesize),
+																			 "Room": self.room,
+																			 "microphone": self.microphone_number,
+																			 "calibration_flag": calibration_flag}})
+				except NewConnectionError as e:
+					self.my_logger.error(f'Couldn\'t resolve address: {e}')
+					tb = traceback.format_exc()
+					self.my_logger.error(tb)
 
-                    # Read file to the head of the post queue to attempt to POST again
-                    temp_q = Queue()
-                    temp_q.put(message)
-                    while not self.post_queue.empty():
-                        temp_q.put(self.post_queue.get())
-                    self.post_queue = temp_q
+					# Read file to the head of the post queue to attempt to POST again
+					temp_q = Queue()
+					temp_q.put(message)
+					while not self.post_queue.empty():
+						temp_q.put(self.post_queue.get())
+					self.post_queue = temp_q
 
-                except Exception as e:
-                    self.my_logger.error(f'\npostCDN exception: {e}\n')
-                    tb = traceback.format_exc()
-                    self.my_logger.error(tb)
+				except Exception as e:
+					self.my_logger.error(f'\npostCDN exception: {e}\n')
+					tb = traceback.format_exc()
+					self.my_logger.error(tb)
 
-                    # Read file to the head of the post queue to attempt to POST again
-                    temp_q = Queue()
-                    temp_q.put(message)
-                    while not self.post_queue.empty():
-                        temp_q.put(self.post_queue.get())
-                    self.post_queue = temp_q
-                """
+					# Read file to the head of the post queue to attempt to POST again
+					temp_q = Queue()
+					temp_q.put(message)
+					while not self.post_queue.empty():
+						temp_q.put(self.post_queue.get())
+					self.post_queue = temp_q
+				"""
 
 
 ##=============================================================================
